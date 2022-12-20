d5bb5fcefe7bbb9ab69eee443b0088675c249e40
Arturo Gonzalez <arturo.gonzalez@rtasoftware.com.mx>
2022-12-20 10:19:35 -0600

    validations on customer dtos added

--- GStation.Core.Models/DTOs/AddressSetDto.cs
+++ GStation.Core.Models/DTOs/AddressSetDto.cs
@@ -1,10 +1,17 @@
-﻿namespace GStation.Core.Models.DTOs
+﻿using GStation.Core.Props.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace GStation.Core.Models.DTOs
 {
     public class AddressSetDto
     {
+        [StringLength(10, ErrorMessage = ValidationConstants.STRING_LENGTH)]
         public string PostalCode { get; set; }
+        [StringLength(30, ErrorMessage = ValidationConstants.STRING_LENGTH)]
         public string Street { get; set; }
+        [StringLength(50, ErrorMessage = ValidationConstants.STRING_LENGTH)]
         public string Subdivision { get; set; }
+        [StringLength(50, ErrorMessage = ValidationConstants.STRING_LENGTH)]
         public string City { get; set; }
         public Guid StateId { get; set; }
     }
--- GStation.Core.Models/DTOs/PersonSignupDto.cs
+++ GStation.Core.Models/DTOs/PersonSignupDto.cs
@@ -1,10 +1,17 @@
-﻿namespace GStation.Core.Models.DTOs
+﻿using GStation.Core.Props.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace GStation.Core.Models.DTOs
 {
     public class PersonSignupDto
     {
+        [StringLength(50, ErrorMessage = ValidationConstants.STRING_LENGTH)]
         public string Name { get; set; }
+        [StringLength(50, ErrorMessage = ValidationConstants.STRING_LENGTH)]
         public string PaternalSurname { get; set; }
+        [StringLength(50, ErrorMessage = ValidationConstants.STRING_LENGTH)]
         public string MaternalSurname { get; set; }
+        [StringLength(15, ErrorMessage = ValidationConstants.STRING_LENGTH)]
         public string PhoneNumber { get; set; }
     }
 }
