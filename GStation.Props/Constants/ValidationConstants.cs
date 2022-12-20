82519d8b8d9d0e9065d5cb05aea2c9455d4e9671
Arturo Gonzalez <arturo.gonzalez@rtasoftware.com.mx>
2022-12-20 10:09:44 -0600

    refactor on data annotations

---
+++ GStation.Props/Constants/ValidationConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace GStation.Core.Props.Constants
+{
+    public static class ValidationConstants
+    {
+        public const string STRING_LENGTH = "El valor de {0} no puede exeder los {1} caracteres.";
+        public const string RANGE = "El valor de {0} debe ser entre {1} y {2}.";
+    }
+}
